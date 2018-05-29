505a0ad5a32e6d060689516acf1953a2a91f35cd
juangod1 <juan.godfrid@gmail.com>
2018-05-29 19:52:19 -0300

    navbar con breadcrumbs anda distintas paginas

--- app/src/main/java/smartcompany/smarthomie/MainActivity.java
+++ app/src/main/java/smartcompany/smarthomie/MainActivity.java
@@ -1,13 +1,57 @@
 package smartcompany.smarthomie;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private HomeFragment homeFragment;
+    private DevicesFragment devicesFragment;
+    private RoutinesFragment routinesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        devicesFragment = new DevicesFragment();
+        routinesFragment = new RoutinesFragment();
+
+        setFragment(homeFragment);
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        setFragment(homeFragment);
+                        break;
+                    case R.id.nav_devices:
+                        setFragment(devicesFragment);
+                        break;
+                    case R.id.nav_routines:
+                        setFragment(routinesFragment);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }

1007ee0ae09a94b41c0752093a1b4428b9d4d692
juangod1 <juan.godfrid@gmail.com>
2018-05-31 20:24:18 -0300

    topbar en el activity y mas diseño

--- app/src/main/java/smartcompany/smarthomie/MainActivity.java
+++ app/src/main/java/smartcompany/smarthomie/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
@@ -32,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
         homeFragment = new HomeFragment();
         devicesFragment = new DevicesFragment();
         routinesFragment = new RoutinesFragment();
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         setFragment(homeFragment);
 

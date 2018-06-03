d00aba51bb3bada70ea872c71bdb7b1347bab281
juangod1 <juan.godfrid@gmail.com>
2018-06-03 19:20:46 -0300

    settings fragment con notif

--- app/src/main/java/smartcompany/smarthomie/SettingsFragment.java
+++ app/src/main/java/smartcompany/smarthomie/SettingsFragment.java
@@ -6,6 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 
 /**
@@ -18,7 +26,6 @@ public class SettingsFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -26,4 +33,41 @@ public class SettingsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ((MainActivity)getActivity()).updateDevices();
+        drawDevices(view);
+    }
+
+    public void addDevices(View view, Device device){
+        TableLayout devices = view.findViewById(R.id.settings_scroll);
+        View settingsView = getLayoutInflater().inflate(R.layout.layout_settings_listitem,((ViewGroup)getView().getParent()),false);
+
+        devices.addView(settingsView);
+
+        TextView t = settingsView.findViewById(R.id.item_name);
+        t.append(device.name);
+
+        CheckBox check = settingsView.findViewById(R.id.checkBox);
+        check.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "check HANDLER WORKS",
+                        Toast.LENGTH_LONG).show();;
+            }
+        });
+    }
+
+    public void drawDevices(View view){
+        MainActivity m = (MainActivity) getActivity();
+        m.updateDevices();
+
+        HashMap<String, Device> rmap = m.getDevicesMap();
+
+        for(String key : rmap.keySet()){
+            addDevices(view, rmap.get(key));
+        }
+    }
 }

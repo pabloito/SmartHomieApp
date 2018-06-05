02506a5a356431a353e331237fb4fe63a1a7e9c1
pabloito <pradnic@itba.edu.ar>
2018-06-05 12:29:36 -0300

    pushear los archivos Gradle le va a romper la repo a juan

--- app/src/main/java/smartcompany/smarthomie/FridgeFragment.java
+++ app/src/main/java/smartcompany/smarthomie/FridgeFragment.java
@@ -1,8 +1,10 @@
 package smartcompany.smarthomie;
 
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -37,19 +40,61 @@ public class FridgeFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         fridge = (Fridge) ((MainActivity)getActivity()).getCurrentDevice();
 
-        final EditText fridgeTempButton = (EditText) view.findViewById(R.id.fridge_temp_button);
-        final EditText refridgeratorTempButton = (EditText) view.findViewById(R.id.refridgerator_temp_button);
+        final SeekBar fridgeTempSlider = (SeekBar) view.findViewById(R.id.fridge_temp_slider);
+        final SeekBar refridgeratorTempSlider = (SeekBar) view.findViewById(R.id.refridgerator_temp_slider);
         final Spinner fridgeModeSelect = (Spinner) view.findViewById(R.id.fridge_mode_select);
         final Button removeButton = (Button) view.findViewById(R.id.fridge_remove_button);
 
+        fridgeTempSlider.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener()
+                {
+                    int min=-20;
+                    int max=-8;
+                    int curr;
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {}
 
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress,
+                                                  boolean fromUser)
+                    {
+                        curr=seekBar.getProgress()-(seekBar.getMax()-max);
+                        System.out.println(curr);
+                    }
+                });
+        refridgeratorTempSlider.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener()
+                {
+                    int min=2;
+                    int max=8;
+                    int curr;
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress,
+                                                  boolean fromUser)
+                    {
+                        curr=seekBar.getProgress()-(seekBar.getMax()-max);
+                        System.out.println(curr);
+                    }
+                });
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),R.array.fridge_mode_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         fridgeModeSelect.setAdapter(adapter);
         fridgeModeSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                System.out.println(selectedItemView);
+                String[] array = getResources().getStringArray(R.array.fridge_mode_array);
+                int index = parentView.getSelectedItemPosition();
+                System.out.println("you have selected: "+array[index]);
+
             }
 
             @Override

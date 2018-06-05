d33b930056d52d2619ca7a03b413bd5ab7373ef5
pabloito <pradnic@itba.edu.ar>
2018-06-05 12:46:46 -0300

    Light and Fridge done

--- app/src/main/java/smartcompany/smarthomie/LightFragment.java
+++ app/src/main/java/smartcompany/smarthomie/LightFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 
 
 /**
@@ -30,5 +35,69 @@ public class LightFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         light = (Light) ((MainActivity)getActivity()).getCurrentDevice();
+
+        final SeekBar lampBrightnessSlider = (SeekBar) view.findViewById(R.id.lamp_brightness_slider);
+        final Spinner lampColorSelect = (Spinner) view.findViewById(R.id.lamp_color_select);
+        final Spinner lampStateSelect = (Spinner) view.findViewById(R.id.lamp_state_select);
+        final Button removeButton = (Button) view.findViewById(R.id.fridge_remove_button);
+
+        lampBrightnessSlider.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener()
+                {
+                    int min=1;
+                    int max=100;
+                    int curr;
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress,
+                                                  boolean fromUser)
+                    {
+                        curr=seekBar.getProgress()-(seekBar.getMax()-max);
+                        System.out.println(curr);
+                    }
+                });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),R.array.lamp_state_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lampStateSelect.setAdapter(adapter);
+        lampStateSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String[] array = getResources().getStringArray(R.array.lamp_state_array);
+                int index = parentView.getSelectedItemPosition();
+                System.out.println("you have selected: "+array[index]);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                System.out.println("No item selected");
+            }
+
+        });
+
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getContext(),R.array.lamp_color_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lampColorSelect.setAdapter(adapter2);
+        lampColorSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                String[] array = getResources().getStringArray(R.array.lamp_color_array);
+                int index = parentView.getSelectedItemPosition();
+                System.out.println("you have selected: "+array[index]);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                System.out.println("No item selected");
+            }
+
+        });
     }
 }

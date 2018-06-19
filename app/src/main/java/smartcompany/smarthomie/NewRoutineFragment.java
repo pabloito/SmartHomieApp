7a6b8c2f4c9a45f500791717d50a3019ed07e7fe
juangod1 <juan.godfrid@gmail.com>
2018-06-19 03:42:23 -0300

    sdfds
    asdf

--- app/src/main/java/smartcompany/smarthomie/NewRoutineFragment.java
+++ app/src/main/java/smartcompany/smarthomie/NewRoutineFragment.java
@@ -100,6 +100,7 @@ public class NewRoutineFragment extends Fragment {
                                 Toast.LENGTH_SHORT);
                         toast.show();
                         ok = false;
+                        return;
                     }
                 }
 

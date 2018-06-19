f053e2a9c283356207be7a3b79b1a7fa7c919adf
nacho9900 <nachonegrocaino@gmail.com>
2018-06-19 03:59:28 -0300

    add new routine fix

--- app/src/main/java/smartcompany/smarthomie/RoutineAction.java
+++ app/src/main/java/smartcompany/smarthomie/RoutineAction.java
@@ -5,17 +5,17 @@ import java.util.List;
 public class RoutineAction {
     private String deviceId;
     private String actionName;
-    private List<Object> parameters;
+    private List<Object> params;
 
-    public RoutineAction(String deviceId, String actionName, List<Object> parameters) {
+    public RoutineAction(String deviceId, String actionName, List<Object> params) {
         this.actionName = actionName;
         this.deviceId = deviceId;
-        this.parameters = parameters;
+        this.params = params;
     }
 
     public void performAction() {
         Device actionDevice = API.GetDeviceById(deviceId);
-        if(actionDevice != null && parameters == null) API.SendEvent(actionDevice,actionName);
-        else if(actionDevice != null)   API.SendEventWithParameters(actionDevice,actionName,parameters);
+        if(actionDevice != null && params == null) API.SendEvent(actionDevice,actionName);
+        else if(actionDevice != null)   API.SendEventWithParameters(actionDevice,actionName, params);
     }
 }
